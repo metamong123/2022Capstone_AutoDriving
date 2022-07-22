05f5d6c8066abf551ef992cf94370205d4bafa16
metamong123 <heeran6179@gmail.com>
2022-07-22 21:51:20 +0900

    core change

--- core/src/core_yaw_gps.py
+++ core/src/core_yaw_gps.py
@@ -62,64 +62,6 @@ def euler_from_quaternion(x, y, z, w):
      
         return yaw_z
 
-#class TopicReciver:
-#	def __init__(self):
-#		self.frenet_sub=rospy.Subscriber("/ackermann_cmd_frenet",AckermannDriveStamped,self.frenet_callback)
-#		self.mode_sub=rospy.Subscriber("/mode_selector",StringArray,self.mode_selector_callback,queue_size=10)
-#		self.YOLO_sub=rospy.Subscriber("/detect_ID", Int32MultiArray, self.yolo_callback)
-#		self.lanenet_sub=rospy.Subscriber("/assist_steer",Float64,self.lanenet_callback)
-#
-#	def check_all_connections(self):
-#		return (self.frenet_sub.get_num_connections()+self.mode_sub.get_num_connections()+self.park_back_sub.get_num_connections())==3
-#		# return (self.frenet_sub.get_num_connections()+self.park_back_sub.get_num_connections()+self.mode_sub.get_num_connections()+self.YOLO_sub.get_num_connections()+self.lanenet_sub.get_num_connections())==5
-#	
-#	def mode_selector_callback(self,msg):
-#		if self.check_all_connections():
-#			global mode_selector, car_mode, move_mode, cur_dir_mode, next_dir_mode
-#
-#			mode_selector = msg.strings
-#			car_mode = mode_selector[0]
-#			move_mode = mode_selector[1]
-#			cur_dir_mode = mode_selector[2]
-#			next_dir_mode = mode_selector[3]
-#			#print("car_mode = ",car_mode, "move_mode = ", move_mode, "cur_dir_mode = ", cur_dir_mode, "next_dir_mode = ", next_dir_mode)
-#
-#	def lanenet_callback(self,msg):
-#		if self.check_all_connections():
-#			global assist_steer
-#			assist_steer = msg.data
-#
-#	def frenet_callback(self,msg):
-#		if self.check_all_connections():
-#			global frenet_speed, frenet_angle, frenet_gear
-#			frenet_speed = msg.drive.speed
-#			if assist_steer == 0:
-#				frenet_angle = msg.drive.steering_angle  
-#			else:
-#				frenet_angle = assist_steer
-#			frenet_gear = 0
-#
-#	def parking_callback(self,msg):
-#		if self.check_all_connections():
-#			global backward_speed, backward_angle, backward_gear, backward_brake
-#			backward_speed = msg.drive.speed
-#			backward_angle = msg.drive.steering_angle
-#			backward_gear = msg.drive.acceleration
-#			backward_brake = msg.drive.jerk
-#
-#	def yolo_callback(self,msg):
-#		if self.check_all_connections():
-#			global deliveryA, deliveryB, traffic_light, person, car, uturnsign, kidzonesign, parkingsign, stopline
-#			deliveryA = msg.data[0]
-#			deliveryB = msg.data[1]
-#			traffic_light = msg.data[2]
-#			person = msg.data[3]
-#			car = msg.data[4]
-#			uturnsign = msg.data[5]
-#			kidzonesign = msg.data[6]
-#			parkingsign = msg.data[7]
-#			stopline = msg.data[8]
-#
 def mode_selector_callback(msg):
 	global mode_selector, car_mode, move_mode, cur_dir_mode, next_dir_mode
     
@@ -181,29 +123,27 @@ def parking_decision():
 	global parking_angle, parking_brake, parking_speed, parking_gear, parking_yaw
  	
 	if parking_flag == 'backward':
-		# if waypoint >= 20:
-		# 	parking_speed = 8/3.6
-		# 	parking_angle = 0
-		# 	parking_gear = 2
-		# 	parking_brake = 0
-		# else:
-		if abs(yaw - parking_yaw) < 0.1:
-			parking_speed = 5/3.6
+		if waypoint >= 20: # hyperparameter
+			parking_speed = 8/3.6
 			parking_angle = 0
 			parking_gear = 2
 			parking_brake = 0
-			if waypoint <= 14:
-				parking_flag = 'end'
-			print('straight')
 		else:
-			parking_speed = 8/3.6
-			parking_angle = -20*np.pi/180
-			parking_gear = 2
-			parking_brake = 0
-			if waypoint <= 14:
+			if abs(yaw - parking_yaw) < 5: # hyperparameter(degree)
+				parking_speed = 0
+				parking_angle = 0
+				parking_gear = 0
+				parking_brake = 100
+				print('parking end')
+				final_cmd_Pub.publish(cmd)
+				rospy.sleep(2)
 				parking_flag = 'end'
-			print('angle')
-		#print('parking backward')
+			else:
+				parking_speed = 8/3.6
+				parking_angle = -20*np.pi/180
+				parking_gear = 2
+				parking_brake = 0
+	
 	else:
 		parking_speed = frenet_speed
 		parking_angle = frenet_angle
@@ -216,12 +156,18 @@ def traffic_decision():
 	global next_dir_mode
 
 	if next_dir_mode == 'left':
-		if traffic_light == 1 or traffic_light == 3 or traffic_light == 5:  # 0 none 1 green 2 left 3 red 4 straightleft 5 yellow
+		if traffic_light == 6 or traffic_light == 8 or traffic_light == 10:  # -1 : none   6 : green   7 : left   8 : red   9 : straightleft   10 : yellow
 			traffic_speed = 0
 			traffic_angle = 0
 			traffic_gear = 0
-			traffic_brake = 200
+			traffic_brake = 100
 			print("traffic mode : stop")
+		elif traffic_light == -1:
+			traffic_speed = frenet_speed/2
+			traffic_angle = frenet_angle
+			traffic_gear = 0
+			traffic_brake = 0
+			print("traffic mode : none")
 		else :
 			traffic_speed = frenet_speed
 			traffic_angle = frenet_angle
@@ -230,12 +176,18 @@ def traffic_decision():
 			print("traffic mode : go")
     
 	elif next_dir_mode == 'straight':
-		if traffic_light == 0 or traffic_light == 3 or traffic_light == 5:
+		if traffic_light == 7 or traffic_light == 8 or traffic_light == 10:
 			traffic_speed = 0
 			traffic_angle = 0
 			traffic_gear = 0
-			traffic_brake = 200
+			traffic_brake = 100
 			print("traffic mode : stop")
+		elif traffic_light == -1:
+			traffic_speed = frenet_speed/2
+			traffic_angle = frenet_angle
+			traffic_gear = 0
+			traffic_brake = 0
+			print("traffic mode : none")
 		else :
 			traffic_speed = frenet_speed
 			traffic_angle = frenet_angle
@@ -243,12 +195,18 @@ def traffic_decision():
 			traffic_brake = 0
 			print("traffic mode : go")
 	elif next_dir_mode == 'right':
-		if traffic_light == 0 or traffic_light == 3 or traffic_light == 5:
+		if traffic_light == 7 or traffic_light == 8 or traffic_light == 10:
 			traffic_speed = 0
 			traffic_angle = 0
 			traffic_gear = 0
-			traffic_brake = 200
+			traffic_brake = 100
 			print("traffic mode : stop")
+		elif traffic_light == -1:
+			traffic_speed = frenet_speed/2
+			traffic_angle = frenet_angle
+			traffic_gear = 0
+			traffic_brake = 0
+			print("traffic mode : none")
 		else :
 			traffic_speed = frenet_speed
 			traffic_angle = frenet_angle
@@ -260,9 +218,7 @@ def traffic_decision():
 if __name__=='__main__':
 
 	rospy.init_node('core_control')
-	#topic_receiver=TopicReciver()
 	rospy.Subscriber("/ackermann_cmd_frenet",AckermannDriveStamped,frenet_callback)
-	#rospy.Subscriber("/ackermann_cmd_parking_backward",AckermannDriveStamped,parking_callback)
 	rospy.Subscriber("/mode_selector",StringArray,mode_selector_callback,queue_size=10)
 	rospy.Subscriber("/detect_ID", Int32MultiArray, yolo_callback)
 	rospy.Subscriber("/assist_steer", Float64, lanenet_callback)
@@ -275,17 +231,26 @@ if __name__=='__main__':
 			if move_mode == 'finish':
 				cmd.drive.speed, cmd.drive.steering_angle, cmd.drive.acceleration, cmd.drive.jerk = traffic_decision()
 			else:
-				if assist_steer == 0:
-					cmd.drive.speed = frenet_speed
-					cmd.drive.steering_angle = frenet_angle
-					cmd.drive.acceleration = frenet_gear
-					cmd.drive.jerk = 0
+				if parking_flag == 'backward':  # for parking
+					cmd.drive.speed, cmd.drive.steering_angle, cmd.drive.acceleration, cmd.drive.jerk = parking_decision()
 				else:
-					cmd.drive.speed = frenet_speed
-					cmd.drive.steering_angle = assist_steer
-					cmd.drive.acceleration = frenet_gear
-					cmd.drive.jerk = 0
-				#print('global mode!!!')
+					if cur_dir_mode == 'straight':
+						if assist_steer == 0:
+							cmd.drive.speed = frenet_speed
+							cmd.drive.steering_angle = frenet_angle
+							cmd.drive.acceleration = frenet_gear
+							cmd.drive.jerk = 0
+						else:
+							cmd.drive.speed = frenet_speed
+							cmd.drive.steering_angle = assist_steer
+							cmd.drive.acceleration = frenet_gear
+							cmd.drive.jerk = 0
+					else:
+						cmd.drive.speed = frenet_speed
+						cmd.drive.steering_angle = frenet_angle
+						cmd.drive.acceleration = frenet_gear
+						cmd.drive.jerk = 0
+					#print('global mode!!!')
 
 		elif car_mode == 'parking':
 			if move_mode == 'forward': # parking forward -> frenet
@@ -305,8 +270,6 @@ if __name__=='__main__':
 				print('parking finish!!! stop!!')
 				rospy.sleep(5) # 5sec
 				parking_flag = 'backward'
-			final_cmd_Pub.publish(cmd)
-		#print(parking_flag)
 		rospy.sleep(0.1)
 		final_cmd_Pub.publish(cmd)
 
