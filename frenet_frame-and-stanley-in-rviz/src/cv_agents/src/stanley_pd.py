13d91748588f110dc10305acf9341891f658dd59
metamong123 <heeran6179@gmail.com>
2022-08-23 18:18:57 +0900

    update stanley pd

--- frenet_frame-and-stanley-in-rviz/src/cv_agents/src/stanley_pd.py
+++ frenet_frame-and-stanley-in-rviz/src/cv_agents/src/stanley_pd.py
@@ -1,6 +1,4 @@
 import numpy as np
-from math import sqrt
-from scipy import interpolate
 
 class Stanley:
 	def __init__(self, k, speed_gain, w_yaw, w_cte,  cte_thresh = 0.5, yaw_dgain = 0, WB = 1.04):
@@ -29,55 +27,62 @@ class Stanley:
 		return angle
 
 
-	def get_map_state(self, front_x, front_y, map_xs, map_ys, map_yaws):
+	def find_nearest_point(self, front_x, front_y, map_xs, map_ys):
 		min_dist = 1e3
-		cte_index = 0
-		yaw_index = 0
+		n_points = len(map_xs)
 
-		if len(map_xs) == 1:
-			cte_index = 0
-			map_yaw = map_yaws[0]
-		else:
-			# get_closest waypoint for cte
-			for i in range(len(map_xs)):
-				dx = front_x - map_xs[i]
-				dy = front_y - map_ys[i]
+		for i in range(n_points):
+			dx = front_x - map_xs[i]
+			dy = front_y - map_ys[i]
 
-				dist = np.sqrt(dx * dx + dy * dy)
-				if dist < min_dist:
-					min_dist = dist
-					cte_index = i
+			dist = np.sqrt(dx * dx + dy * dy)
+			if dist < min_dist:
+				min_dist = dist
+				min_index = i
+		
+		return min_index
 
-			# get yaw_index for yaw_term
-			map_vec = np.array([map_xs[cte_index + 1] - map_xs[cte_index], map_ys[cte_index + 1] - map_ys[cte_index]])
-			ego_vec = np.array([front_x - map_xs[cte_index], front_y - map_ys[cte_index]])
-			direction  = np.sign(np.dot(map_vec, ego_vec))
+    
+	def stanley_control(self, x, y, yaw, v, map_xs, map_ys, map_yaws):
+		front_x = x + self.WB/2*np.cos(yaw)
+		front_y = y + self.WB/2*np.sin(yaw)
 
-			if direction < 0:
-				yaw_index = cte_index - 1
-			else:
-				yaw_index = cte_index
+		# find nearest point
+		min_index = self.find_nearest_point(front_x, front_y, map_xs, map_ys)
+        
+		map_x = map_xs[min_index]
+		map_y = map_ys[min_index]
+		map_yaw = map_yaws[min_index]
+		dx = map_x - front_x
+		dy = map_y - front_y
 
-			# get map_yaw: linear interpolation
-			map_dist = sqrt((map_xs[yaw_index + 1] - map_xs[yaw_index])**2 + (map_ys[yaw_index + 1] - map_ys[yaw_index])**2)
-			map_car_dist = abs(np.dot(map_vec/map_dist, ego_vec))
+		# compute cte at front axle
+		perp_vec = [np.cos(yaw + np.pi/2), np.sin(yaw + np.pi/2)]
+		cte = np.dot([dx, dy], perp_vec)
 
-			yaw_linear = interpolate.interp1d([0, map_dist], [map_yaws[yaw_index], map_yaws[yaw_index+1]], kind='linear')
-			map_yaw = yaw_linear(map_car_dist)
+		# control law
+		yaw_term = self.normalize_angle(map_yaw - yaw) # heading error
+		cte_term = np.arctan2(self.k*cte , (self.speed_gain + v)) # cross track error
 
-		return map_xs[cte_index], map_ys[cte_index], map_yaw
+		# steering
+		steer = self.w_yaw * yaw_term + self.w_cte * cte_term
+		return steer, yaw_term, cte
 
 
 	def stanley_control_pd(self, x, y, yaw, v, map_xs, map_ys, map_yaws):
 		front_x = x + self.WB/2*np.cos(yaw)
 		front_y = y + self.WB/2*np.sin(yaw)
 
-		# get map state of closest waypoint
-		map_x, map_y, map_yaw = self.get_map_state(front_x, front_y, map_xs, map_ys, map_yaws)
+		# find nearest point
+		min_index = self.find_nearest_point(front_x, front_y, map_xs, map_ys)
 
-		# compute cte at front axle
+		map_x = map_xs[min_index]
+		map_y = map_ys[min_index]
+		map_yaw = map_yaws[min_index]
 		dx = map_x - front_x
 		dy = map_y - front_y
+
+		# compute cte at front axle
 		perp_vec = [np.cos(map_yaw + np.pi/2), np.sin(map_yaw + np.pi/2)]
 		cte = np.dot([dx, dy], perp_vec)
 
